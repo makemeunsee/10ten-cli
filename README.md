f652e091f04288bbc93a2207e18b0c3fa0411608
makemeunsee <marc-antoine.galilee@etik.com>
2024-04-15 17:24:05 +0200

    typescript CLI

---
+++ README.md
@@ -0,0 +1,25 @@
+# Typescript CLI, 10ten to be
+
+## Building
+
+```sh
+npm run build
+# or
+npx tsc
+```
+
+## Installing
+
+```sh
+npm install . -g
+```
+
+## Running
+
+```sh
+# after installing
+ten10-cli
+
+# or after just building
+node dist/index.js
+```
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+const { Command } = require("commander");
+const figlet = require("figlet");
+const fs = require("fs");
+const path = require("path");
+
+const program = new Command();
+
+console.log(figlet.textSync("Dir Manager"));
+
+program
+  .version("1.0.0")
+  .description("An example CLI for managing a directory")
+  .option("-l, --ls  [value]", "List directory contents")
+  .option("-m, --mkdir <value>", "Create a directory")
+  .option("-t, --touch <value>", "Create a file")
+  .parse(process.argv);
+
+const options = program.opts();
+
+async function listDirContents(filepath: string) {
+  try {
+    const files = await fs.promises.readdir(filepath);
+    const detailedFilesPromises = files.map(async (file: string) => {
+      let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
+      const { size, birthtime } = fileDetails;
+      return { filename: file, "size": size, created_at: birthtime };
+    });
+    const detailedFiles = await Promise.all(detailedFilesPromises);
+    console.table(detailedFiles);
+  } catch (error) {
+    console.error("Error occurred while reading the directory!", error);
+  }
+}
+
+if (options.ls) {
+  const filepath = typeof options.ls === "string" ? options.ls : __dirname;
+  listDirContents(filepath);
+}
